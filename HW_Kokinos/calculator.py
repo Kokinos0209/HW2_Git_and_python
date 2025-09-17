23f8001826857c2eb47865cb80923da3deb0747b
Nastya <nastya.gorenkova.2016@mail.ru>
2025-09-17 22:24:48 +0300

    add main function

--- HW_Kokinos/calculator.py
+++ HW_Kokinos/calculator.py
@@ -1,2 +1,18 @@
-def addition(a, b):
-    return (a + b)
+def main(expression):
+    parts = expression.split()
+    operator = parts[1]
+    num1, num2 = float(parts[0]), float(parts[2])
+    result = 0
+    if operator == '+':
+        result = addition(num1, num2)
+    elif operator == '-':
+        result = difference(num1, num2)
+    elif operator == '*':
+        result = multiple(num1, num2)
+    elif operator == '/':
+        if num2==0:
+            return 'На ноль делить нельзя!'
+        result = deviation(num1, num2)
+    return result
+
+print(main('5 / 0'))
